e862d548be4b201330682fe9b0c5e0aaea5cef92
Noci <nocigit@gmail.com>
2021-10-21 15:31:05 +0200

    Added getModDir()

--- src/main/java/net/tttprojekt/cascademode/Installer.java
+++ src/main/java/net/tttprojekt/cascademode/Installer.java
@@ -8,37 +8,34 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.SplittableRandom;
 
 
 public class Installer {
 
     private static final Logger logger = LoggerFactory.getLogger(Installer.class);
-    private static final String MODS_FOLDER;
-
-    static {
-        String modsFolder;
-        String OS = (System.getProperty("os.name")).toUpperCase();
-        if (OS.contains("WIN")) {
-            modsFolder = System.getenv("AppData");
-            modsFolder = modsFolder.replaceAll("\\\\", "/");
-        } else {
-            modsFolder = System.getProperty("user.home");
-            modsFolder += "/Library/Application Support";
-
-        }
-
-        modsFolder += "/.minecraft/mods";
-        MODS_FOLDER = modsFolder;
-    }
-
+    
     private static final String MOD_DOWNLOAD_URL = "https://github.com/xNoci/CascadeMod-Installer/blob/dev/modfile/Cascade%20Mod.jar?raw=true";
     private static final String OPTIFINE_DOWNLOAD_URL = "https://optifine.net/downloadx?f=OptiFine_1.12.2_HD_U_G5.jar&x=95dd0de8fe2ef755e347876857646d28";
     private static final String JEI_DOWNLOAD_URL = "https://media.forgecdn.net/files/3040/523/jei_1.12.2-4.16.1.301.jar";
 
+    private static final String MODS_FOLDER = getModDir().getAbsolutePath();
+
     private static final String FILE_DESTINATION_MOD = MODS_FOLDER + "/Cascade Mod.jar";
     private static final String FILE_DESTINATION_OPTIFINE = MODS_FOLDER + "/OptiFine_1.12.2_HD_U_G5.jar";
     private static final String FILE_DESTINATION_JEI = MODS_FOLDER + "/jei_1.12.2-4.16.1.301.jar";
+
+    private static File getModDir() {
+        String userHomeDir = System.getProperty("user.home", ".");
+        String osType = System.getProperty("os.name").toLowerCase(Locale.ENGLISH);
+        String mcDir = ".minecraft/mods";
+        if (osType.contains("win") && System.getenv("APPDATA") != null)
+            return new File(System.getenv("APPDATA"), mcDir);
+        if (osType.contains("mac"))
+            return new File(new File(new File(userHomeDir, "Library"), "Application Support"), "minecraft");
+        return new File(userHomeDir, mcDir);
+    }
 
     private DownloadTaskManager downloadTaskManager;
     private DownloadTask modDownloadTask;
