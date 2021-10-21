982d2433e5b0dc5024bc9af36a8f80f51724aabb
Noci <nocigit@gmail.com>
2021-10-21 13:57:12 +0200

    Print information on start

---
+++ src/main/java/net/tttprojekt/cascademode/Main.java
@@ -0,0 +1,34 @@
+package net.tttprojekt.cascademode;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        printInformation();
+
+    }
+
+
+    private static void printInformation() {
+        logger.info("");
+        logger.info("-------------------------------------------------------------------------------");
+        logger.info("                      Starting TTT-MP Mod-Installer");
+        logger.info("                                by Noci");
+        logger.info("-------------------------------------------------------------------------------");
+        logger.info("");
+        logger.info(String.format("Running Java Version %s", System.getProperty("java.version")));
+        logger.info(String.format("Application is running on %s (%s)", System.getProperty("os.name"), System.getProperty("os.arch")));
+        logger.info(String.format("Operation System Version: %s", System.getProperty("os.version")));
+        logger.info("");
+        logger.info("-------------------------------------------------------------------------------");
+        logger.info("    >>>>>>>>> This installer installs you forge and the needed mods <<<<<<<<<  ");
+        logger.info("-------------------------------------------------------------------------------");
+        logger.info("");
+    }
+
+}
