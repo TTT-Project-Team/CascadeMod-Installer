e0f97a747277a1206aa72f44f79c2f9c5ead2891
Noci <nocigit@gmail.com>
2021-10-25 09:23:51 +0200

    Added OptiFineFetcher to fetch a OF download link

---
+++ src/main/java/net/tttprojekt/cascademode/utils/OptiFineVersion.java
@@ -0,0 +1,21 @@
+package net.tttprojekt.cascademode.utils;
+
+import lombok.Getter;
+
+public enum OptiFineVersion {
+
+    V_1_12_2_HD_U_G5("OptiFine_1.12.2_HD_U_G5");
+
+
+    private static final String OPTIFINE_URL = "http://optifined.net/adloadx.php?f=%s.jar";
+    @Getter private final String version;
+
+    OptiFineVersion(String version) {
+        this.version = version;
+    }
+
+    public String getLink() {
+        return String.format(OPTIFINE_URL, this.version);
+    }
+
+}
