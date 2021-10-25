df7827a28bd85ff567f14d81146cda768816031e
Noci <nocigit@gmail.com>
2021-10-25 09:40:47 +0200

    Added MinecraftChecker

---
+++ src/main/java/net/tttprojekt/cascademode/utils/MinecraftChecker.java
@@ -0,0 +1,17 @@
+package net.tttprojekt.cascademode.utils;
+
+import java.util.List;
+
+public class MinecraftChecker {
+
+    private static final String MINECRAFT_PROCESS = "net.minecraft.launchwrapper.Launch";
+
+    public static boolean isMinecraftRunning() {
+        Process process = ProcessUtils.getProcess("jps", "-l");
+        List<String> input = ProcessUtils.getProcessInput(process);
+
+        return input.stream().anyMatch(processInfo -> processInfo.toLowerCase().contains(MINECRAFT_PROCESS.toLowerCase()));
+    }
+
+
+}
