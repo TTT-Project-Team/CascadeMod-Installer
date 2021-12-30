c0a38bc1231e7a4e3ffe6c7475e7657beb571745
Noci <nocigit@gmail.com>
2021-12-30 10:41:03 +0100

    Added Download enum for getting download tasks

---
+++ src/main/java/net/tttprojekt/cascademode/download/Download.java
@@ -0,0 +1,28 @@
+package net.tttprojekt.cascademode.download;
+
+import net.tttprojekt.cascademode.utils.FileDestination;
+import net.tttprojekt.cascademode.utils.OptiFineFetcher;
+import net.tttprojekt.cascademode.utils.OptiFineVersion;
+
+public enum Download {
+
+    CASCADE_MOD(FileDestination.getCascadeMod(), "https://github.com/xNoci/CascadeMod-Installer/blob/dev/modfile/Cascade%20Mod.jar?raw=true"),
+    JEI(FileDestination.getJustEnoughItems(), "https://media.forgecdn.net/files/3040/523/jei_1.12.2-4.16.1.301.jar"),
+    OPTIFINE(FileDestination.getOptiFine(), OptiFineVersion.V_1_12_2_HD_U_G5),
+    FORGE(FileDestination.getForgeInstallerFile(), "https://maven.minecraftforge.net/net/minecraftforge/forge/1.12.2-14.23.5.2855/forge-1.12.2-14.23.5.2855-installer.jar");
+
+    private final DownloadTask downloadTask;
+
+    Download(String destination, String url) {
+        this.downloadTask = DownloadTask.of(url, destination);
+    }
+
+    Download(String destination, OptiFineVersion optiFineVersion) {
+        this.downloadTask = OptiFineFetcher.getDownloadTask(destination, optiFineVersion);
+    }
+
+    DownloadTask get() {
+        return this.downloadTask;
+    }
+
+}
